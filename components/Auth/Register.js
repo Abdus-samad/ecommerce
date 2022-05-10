a87c7d13daa1af5b09d3b6114cfc9b4ce51194a4
Abdus-samad <abdussamadajao@gmail.com>
2022-05-10 21:32:49 +0100

    Feat/database

--- components/Auth/Register.js
+++ components/Auth/Register.js
@@ -5,7 +5,7 @@ import { useAuth } from "../../context/Auth/auth";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../context/Auth/firebaseClient";
 
-const SignUp = () => {
+const Register = () => {
   const { SwitchToLogin } = useContext(loginContext);
   const { signWithGoogle } = useAuth();
 
@@ -35,49 +35,47 @@ const SignUp = () => {
       <ToastContainer />
       <form onSubmit={onSubmit}>
         <input
-          type="email"
-          placeholder="Email"
-          className="block w-full h-12 px-5 mb-3 text-base text-gray-800 transition-all duration-100 bg-gray-300 border border-gray-300 rounded ease"
+          type='email'
+          placeholder='Email'
+          className='block w-full h-12 px-5 mb-3 text-base text-gray-800 transition-all duration-100 bg-gray-300 border border-gray-300 rounded ease'
           value={email}
-          name="email"
+          name='email'
           onChange={onChange}
           required
         />
         <input
-          type="password"
-          placeholder="password"
-          className="block w-full h-12 px-5 mb-3 text-base text-gray-800 transition-all duration-100 bg-gray-300 border border-gray-300 rounded ease"
+          type='password'
+          placeholder='password'
+          className='block w-full h-12 px-5 mb-3 text-base text-gray-800 transition-all duration-100 bg-gray-300 border border-gray-300 rounded ease'
           value={password}
-          name="password"
+          name='password'
           onChange={onChange}
           required
         />
-        <p className="w-full mb-2 text-xs font-medium text-center text-gray-800 pt-[20px]">
+        <p className='w-full mb-2 text-xs font-medium text-center text-gray-800 pt-[20px]'>
           By signing up, you agree to Pickbazar's <br />
-          <a className="font-bold text-blue-800 underline">Terms & Condtion</a>
+          <a className='font-bold text-blue-800 underline'>Terms & Condtion</a>
         </p>
-        <button className="flex items-center justify-center flex-shrink-0 w-full h-12 px-8 text-base font-bold text-center text-white transition-all duration-100 border-0 rounded cursor-pointer bg-secondary-main ease">
+        <button className='flex items-center justify-center flex-shrink-0 w-full h-12 px-8 text-base font-bold text-center text-white transition-all duration-100 border-0 rounded cursor-pointer bg-secondary-main ease'>
           {" "}
           Continue
         </button>
       </form>
-      <div className="relative flex items-center justify-center w-full py-4">
-        <span className="relative px-3 text-base font-medium text-gray-800">
+      <div className='relative flex items-center justify-center w-full py-4'>
+        <span className='relative px-3 text-base font-medium text-gray-800'>
           or
         </span>
       </div>
       <button
         onClick={() => signWithGoogle()}
-        className="flex items-center justify-center flex-shrink-0 w-full h-12 px-8 mb-4 text-base font-bold text-center text-white transition-all duration-100 bg-blue-600 border-0 rounded cursor-pointer ease"
-      >
+        className='flex items-center justify-center flex-shrink-0 w-full h-12 px-8 mb-4 text-base font-bold text-center text-white transition-all duration-100 bg-blue-600 border-0 rounded cursor-pointer ease'>
         Continue with Google
       </button>
-      <p className="p-5 text-base font-medium text-gray-500">
+      <p className='p-5 text-base font-medium text-gray-500'>
         Already have an account?
         <button
           onClick={SwitchToLogin}
-          className="font-bold text-green-700 underline bg-transparent border-0 cursor-pointer otline-0"
-        >
+          className='font-bold text-green-700 underline bg-transparent border-0 cursor-pointer otline-0'>
           Login
         </button>
       </p>
@@ -85,4 +83,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default Register;
